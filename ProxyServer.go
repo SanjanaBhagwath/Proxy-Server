ef633437306447233f4053da7a0b25febb7d2923
Sanjana Bhagwath <sanjanabhagwath0504@gmail.com>
2025-05-29 14:50:27 +0530

    Removed API key

--- ProxyServer.go
+++ ProxyServer.go
@@ -14,7 +14,7 @@ type proxyHandler struct{}
 
 func isSafeToBrowse(url string) bool {
 	ctx := context.Background()
-	apiKey := "AIzaSyCLZVrO9jCsnhuQv2PTEL5J-0GoqTLWOBc"
+	apiKey := ""
 
 	// Create Safe Browsing service
 	safeBrowsingService, err := safebrowsing.NewService(ctx, option.WithAPIKey(apiKey))
